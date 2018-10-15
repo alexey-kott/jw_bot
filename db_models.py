c0f8f1a2a4a155514b393a364925b1b927944ef8
Alexey Kott <alexey.kott@gmail.com>
2018-10-16 01:33:08 +0300

    Parsing journals

--- db_models.py
+++ db_models.py
@@ -1,9 +1,16 @@
-from peewee import Model, SqliteDatabase
+from peewee import Model, SqliteDatabase, TextField, DateTimeField, IntegerField
 
 db = SqliteDatabase('bot.db')
 
-class Journal(Model):
 
+class Publication(Model):
+    publication_type = TextField()
+    publication_number = IntegerField()
+    publication_year = IntegerField()
+    url = TextField()
+    telegraph_url = TextField()
+    parsed_date = DateTimeField()
+    text = TextField()
 
     class Meta:
         database = db
--- parser.py
+++ parser.py
@@ -1,11 +1,40 @@
 import asyncio
 
 from aiohttp import ClientSession
+from bs4 import BeautifulSoup
 
+
+MAIN_URL = "https://www.jw.org/ru"
+
+
+async def parse_journals_per_year(session: ClientSession, year: str):
+    params = {
+        'contentLanguageFilter': 'ru',
+        'pubFilter': '',
+        'yearFilter': year
+    }
+    async with session.get(f"{MAIN_URL}/публикации/журналы/", params=params) as response:
+        soup = BeautifulSoup(await response.text(), 'lxml')
+
+        journal_links = soup.find_all("div", class_="publicationDesc")
+        for i in journal_links:
+            print(i)
+
+
+
+async def parse_journals(session: ClientSession):
+    async with session.get(f"{MAIN_URL}/публикации/журналы/") as response:
+        journals_page = await response.text()
+
+        soup = BeautifulSoup(journals_page, 'lxml')
+        pub_filters = soup.find("select", {"id": "yearFilter"})
+        for option in pub_filters.find_all("option", {'value': lambda x: x != ''}):
+            await parse_journals_per_year(session, option['value'])
 
 
 async def main():
     async with ClientSession() as session:
+        await parse_journals(session)
 
 
 if __name__ == "__main__":
